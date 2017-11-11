c4e8fb54ac09f4d8631079332bbef1d314d4ddbe
ben mishali <ben9992@gmail.com>
2017-11-11 14:43:08 +0200

    fsdf

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world!!! - Now edit from GitHub");
+            Console.WriteLine("hello world!!! - Now edit from GitHub and now from");
         }
     }
 }
